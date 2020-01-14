5e99aefc206c367f155438020a302e97a0a68965
dimitos <kurort26@mail.ru>
2020-01-14 16:34:59 +0300

    сделал функцию

--- task1.php
+++ task1.php
@@ -3,19 +3,33 @@
 // Обязательно доделаю
 // Мне кажется тут нужно слепить функцию, ща попробую
 
-//function listDir(){
-//    return
-//}
+function listDir($id_dir){
+    //формируем путь
+    $strDir = '/' . $id_dir;
 
-
-$arr = scandir('/');
-array_shift($arr);
-var_dump($arr);
-$str = '';
-foreach($arr as $key=>$value) {
-    $str = $str . '<a href="task1-1.php?id=' . $value . '">' . $value . '</a><br>';
+    // сканировали новый путь
+    $arr = scandir($strDir);
+    // выводим список
+    $str = '';
+    foreach($arr as $key=>$value) {
+        $str = $str . '<a href="?id=' . $value . '">' . $value . '</a><br>';
+    }
+    return $str;
 }
-echo $str;
+
+$id = $_GET['id'];
+echo listDir($id);
+
+
+
+
+//array_shift($arr);
+//var_dump($arr);
+//$str = '';
+
+//echo $str;
+
+//empty($_GET['id']) ? exit : listDir($id);
 
 //var_dump($_GET);
 //$previous = $_GET['id'];
