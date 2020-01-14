943fa40745a542718a4fad6a21eaed8f94c44d34
dimitos <kurort26@mail.ru>
2020-01-14 15:56:55 +0300

    !

--- task1.php
+++ task1.php
@@ -1,9 +1,11 @@
 <?php
+// Здесь я в процессе.
+// Обязательно доделаю
+// Мне кажется тут нужно слепить функцию, ща попробую
+
 //function listDir(){
 //    return
 //}
-
-
 
 
 $arr = scandir('/');
@@ -14,4 +16,15 @@ foreach($arr as $key=>$value) {
     $str = $str . '<a href="task1-1.php?id=' . $value . '">' . $value . '</a><br>';
 }
 echo $str;
+
+//var_dump($_GET);
+//$previous = $_GET['id'];
+//$dir = '/' . $_GET['id'] . '/';
+//$arr = scandir($dir);
+//
+//foreach($arr as $key=>$value) {
+//    echo <<<php
+//        <a href="?id=$value">$value</a><br>
+//php;
+//}
 
